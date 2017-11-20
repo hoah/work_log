fe6ba871055a4715edc5357d78908c3d1ddaa9ef
Hoa Huynh <hoa.h.huynh@gmail.com>
2017-11-20 12:05:20 -0600

    Start  work_log project

---
+++ work_log.py
@@ -0,0 +1,13 @@
+import csv
+import datetime
+import os
+import re
+
+
+def clear():
+    '''Clears console.'''
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+if __name__ == '__main__':
+    pass
